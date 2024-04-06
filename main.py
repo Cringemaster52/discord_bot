887a23b5f25a167bdda083e270292f6b2f1b9e3b
Cringemaster52 <144527761+Cringemaster52@users.noreply.github.com>
2024-04-06 11:58:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,28 +17,24 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!' 
-                   '\n$heh - повторяет "he" введенное кол-во раз'
-                   '\n$repeat - повторяет слово введенное кол-во раз'
-                   '\n$choose - выбирает между введенными вариантами'
                    '\n$mem - присылает мем'
-                   '\n$duck - gif с уточками'
-                   '\n$fox - gif с лисичками'
+                   '\n$facts - присылает факты'
                    '\n$eco - советы про экологию')
 
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+#@bot.command()
+#async def heh(ctx, count_heh = 5):
+#    await ctx.send("he" * count_heh)
 
-@bot.command()
-async def repeat(ctx, times:int, content='repeating...'):
-    """reapeats a message a multiple times"""
-    for i in range(times):
-        await ctx.send(content)
+#@bot.command()
+#async def repeat(ctx, times:int, content='repeating...'):
+#    """reapeats a message a multiple times"""
+#    for i in range(times):
+#        await ctx.send(content)
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
+#@bot.command(description='For when you wanna settle the score some other way')
+#async def choose(ctx, *choices: str):
+#    """Chooses between multiple choices."""
+#    await ctx.send(random.choice(choices))
 
 @bot.command()
 async def mem(ctx):
@@ -49,30 +45,30 @@ async def mem(ctx):
    # Можем передавать файл как параметр!
     await ctx.send(file=picture)
 
-def get_duck_image_url():    
-    url = 'https://random-d.uk/api/random'
-    res = requests.get(url)
-    data = res.json()
-    return data['url']
+#def get_duck_image_url():    
+#    url = 'https://random-d.uk/api/random'
+#    res = requests.get(url)
+#    data = res.json()
+#    return data['url']
 
 
-@bot.command('duck')
-async def duck(ctx):
-    '''По команде duck вызывает функцию get_duck_image_url'''
-    image_url = get_duck_image_url()
-    await ctx.send(image_url)
+#@bot.command('duck')
+#async def duck(ctx):
+#    '''По команде duck вызывает функцию get_duck_image_url'''
+#    image_url = get_duck_image_url()
+#    await ctx.send(image_url)
 
-def get_fox_image_url():
-    url = 'https://randomfox.ca/floof/'
-    res = requests.get(url)
-    data = res.json()
-    return data['image']
+#def get_fox_image_url():
+#    url = 'https://randomfox.ca/floof/'
+#    res = requests.get(url)
+#    data = res.json()
+#    return data['image']
 
-@bot.command('fox')
-async def fox(ctx):
-    '''По команде duck вызывает функцию get_duck_image_url'''
-    image_url = get_fox_image_url()
-    await ctx.send(image_url)
+#@bot.command('fox')
+#async def fox(ctx):
+#    '''По команде duck вызывает функцию get_duck_image_url'''
+#    image_url = get_fox_image_url()
+ #   await ctx.send(image_url)
 
 list = ['Найдите ближайший к вам контейнер, куда сможете относить мусор для переработки.', 'Обращайте внимание на вид упаковки еще в магазине.',
 'Помните, что отсортированный мусор должен быть чистым.',
